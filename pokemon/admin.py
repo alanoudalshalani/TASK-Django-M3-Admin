203e2a6e94f3b00a5c234709ccd29669347e4140
alanoudalshalani <107769859+alanoudalshalani@users.noreply.github.com>
2022-10-16 18:39:30 +0300

    all set

--- pokemon/admin.py
+++ pokemon/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Pokemon
 
-# Register your models here.
+
+@admin.register(Pokemon)
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name' , 'hp', "active")
+
+
+
+
+
+
--- pokemon/models.py
+++ pokemon/models.py
@@ -1,3 +1,29 @@
+from email.policy import default
+from queue import Empty
+from re import T
+from xmlrpc.client import DateTime
 from django.db import models
 
-# Create your models here.
+
+class Pokemon(models.Model):
+    class PokemonType(models.TextChoices):
+        WATER = 'WA'
+        GRASS = 'GR'
+        GHOST = 'GH'
+        STEEL = 'ST'
+        FAIRY = 'FA'
+    name = models.CharField(max_length=30)
+    type = models.CharField(max_length=30,choices=PokemonType.choices, default="WATER")
+    hp = models.PositiveBigIntegerField
+    active = models.BooleanField(default=True)
+    name_fr = models.CharField(max_lenght=30 , default=Empty , blank = True)
+    name_ar = models.CharField(max_lenght=30 , default=Empty , blank = True)
+    name_jp = models.CharField(max_lenght=30 , default=Empty , blank = True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__ (self):
+        return self.name
+
+
+
